60b03ad6be22cf54c6a85bbf1a8af92f2086a106
ryanflorin <ryan.a.florin@gmail.com>
2016-04-27 10:08:49 -0400

    Initial commit

---
+++ car.cpp
@@ -0,0 +1,110 @@
+//File:         VehicleParking.cpp
+//Project name: To simulate the datacenter for a vehicle parking lot in an Airport.
+//Description:  The goal of the project is to simulate a datacenter implemented on the cars in the parking lot of a medium sized airport.
+//              As cars arrive and depart randomly, the challenge facing the implementation of the datacenter is to maintain high availability
+//              and reliability in the face of the dynamically changing resources. Tradeoffs will be identified and analyzed and several
+//              possible solutions will be contrasted. The project is deliberately open ended, allowing each student to add performance-enhancing
+//              "bells and whistles".
+//Author1:      Aida Ghazizadeh
+//Email:        aghaziza@cs.odu.edu
+//Author2:      Srinivas Havanur
+//Email:        shavanur@cs.odu.edu
+//Submitted to: Dr. Stephen Olariu
+//Submitted on: 04 December 2015
+//Language:     C++
+
+
+
+car::car()
+{
+
+  // This is to keep track how many cars turned away when they try to occupy the parking space. Initially set to -1.
+  // turned_away=0 - Car didn't turn away or got parking space.
+  // turned_away=1 - Car turned away
+  turned_away=-1;                                        
+
+  car_spot_number=-1;
+  job_has_been_migrated=false;
+  car_flag_start_migration=false;
+  car_flag_end_migration=false;
+  car_num_migration_to = -1;
+  print_migration_flag_counter=0;
+}
+
+// Displays all car information including the spot number to which the car belongs to. Initially when the simulation starts
+// all the parking spots will be occupied by cars.
+void car::display_car_information(std::ofstream& outFile)                  
+{                                                                
+  outFile<<"\n";
+  outFile<<"-----------------------------------------"<<"\n";
+  outFile<<"		Car Details		        "<<"\n";
+  outFile<<"-----------------------------------------"<<"\n";
+  outFile<<"car number : "<<car_number<<"\n";
+  outFile<<"car spot number : "<<car_spot_number<<"\n";
+  outFile<<"job number assigned to this car : "<<job_number<<"\n";
+  outFile<<"departure time of this car : "<<departure_time<<"\n";
+  outFile<<"arrival time of this car : "<<arrival_time_of_car<<"\n";
+  outFile<<"The Residency time: "<<residency_time<<"\n";
+  outFile<<"Car Turned away: "<<turned_away<<"\n";
+}
+
+
+//Calculates the departure time of a car when simulation starts. This is between 1-2560 cars
+// min residency time of a car is 24 hours (1440 minutes) and max residency time is 7 days (10080 minutes)
+// this function is for cars that are already in the parking lot when we start our simulation
+void car::calculate_departure_time()
+{                                
+  departure_time= rand()%(10080-1440)+1440;                          
+}																     
+
+
+// Car arrives 60 minutes earlier to the flight departure hence the arrival time.
+void car::get_arrival_time_of_car(std::ifstream& inFile)
+{
+  inFile>>departure_time_of_flight;
+  arrival_time_of_car = departure_time_of_flight - 60;
+}
+
+// Get the departure time of car between 2561 to 5000 cars by making use of arrival time of a flight.														
+//The departure time is 60 minutes after the arrival.
+void car::get_departure_time_of_car(std::ifstream& inFile)
+{	
+  inFile>>arrival_time_of_flight;
+  departure_time = arrival_time_of_flight + 60;
+}
+
+//Calculates the residency time of a car based on the arrival and departure time of a car.
+void car::calculate_residency_time_of_car()                          
+{
+  residency_time = departure_time - arrival_time_of_car;
+}
+
+
+//This displays the details of a car which may fail if we don't do the migration.
+void car::display_failed_job_car_details(std::ofstream& outFileFailed)    
+{
+  outFileFailed<<"\n";
+  outFileFailed<<"-----------------------------------------"<<"\n";
+  outFileFailed<<"		Failed Job Car Details		        "<<"\n";
+  outFileFailed<<"-----------------------------------------"<<"\n";
+  outFileFailed<<"car number : "<<car_number<<"\n";
+  //outFileFailed<<"car spot number : "<<car_spot_number<<"\n";
+  outFileFailed<<"job number assigned to this car : "<<job_number<<"\n";
+  outFileFailed<<"departure time of this car : "<<departure_time<<"\n";
+  outFileFailed<<"arrival time of this car : "<<arrival_time_of_car<<"\n";
+}
+
+
+//This displays the details of a car which may pass during the simulation.
+void car::display_pass_job_car_details(std::ofstream& outFilePassJob)    
+{
+  outFilePassJob<<"\n";
+  outFilePassJob<<"-----------------------------------------"<<"\n";
+  outFilePassJob<<"		Pass Job Car Details		        "<<"\n";
+  outFilePassJob<<"-----------------------------------------"<<"\n";
+  outFilePassJob<<"car number : "<<car_number<<"\n";
+  //outFilePassJob<<"car spot number : "<<car_spot_number<<"\n";
+  outFilePassJob<<"job number assigned to this car : "<<job_number<<"\n";
+  outFilePassJob<<"departure time of this car : "<<departure_time<<"\n";
+  outFilePassJob<<"arrival time of this car : "<<arrival_time_of_car<<"\n";
+}
---
+++ job.cpp
@@ -0,0 +1,52 @@
+//File:         VehicleParking.cpp
+//Project name: To simulate the datacenter for a vehicle parking lot in an Airport.
+//Description:  The goal of the project is to simulate a datacenter implemented on the cars in the parking lot of a medium sized airport.
+//              As cars arrive and depart randomly, the challenge facing the implementation of the datacenter is to maintain high availability
+//              and reliability in the face of the dynamically changing resources. Tradeoffs will be identified and analyzed and several
+//              possible solutions will be contrasted. The project is deliberately open ended, allowing each student to add performance-enhancing
+//              "bells and whistles".
+//Author1:      Aida Ghazizadeh
+//Email:        aghaziza@cs.odu.edu
+//Author2:      Srinivas Havanur
+//Email:        shavanur@cs.odu.edu
+//Submitted to: Dr. Stephen Olariu
+//Submitted on: 04 December 2015
+//Language:     C++
+
+job::job()
+{
+  job_flag_start_migration = false;
+  job_flag_end_migration = false;
+  data_replication_time_to_same_cluster=0;
+  data_replication_time_to_same_region=0;
+  data1_car_number = -1;
+  data2_car_number = -1;
+}
+
+//This displays the job information which is assigned to all the cars in the parking lot as soon as simulation starts.
+void job::display_job_information(std::ofstream& outFile)
+{
+  outFile<<"\n";
+  outFile<<"-----------------------------------------"<<"\n";
+  outFile<<"		Job Details		        "<<"\n";
+  outFile<<"-----------------------------------------"<<"\n";
+  outFile<<"job number : "<<job_number<<"\n";
+  outFile<<"job size : "<<VM_size<<"\n";
+  outFile<<"car number that this job is assigned to : "<<car_number<<"\n";
+}
+
+
+
+void job::calculate_VM_size(){
+  // our jobs are between 1 GB and 5 GB
+  VM_size= rand()%(5000-1000)+1000;                           
+  VM_migration_remained = VM_size;
+}
+
+// Calculates the time it takes to complete a job in a car.
+void job::calculate_job_duration(){                                          
+  //we assume that 1 MB takes 1/10 minute to finish 
+  job_duration= (VM_size*0.1);												 
+  job_duration_remained = job_duration;
+}																
+
